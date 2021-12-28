eb0eea8a27a8e2abc47e70e8445053f86cec26da
Yogesh kumar <yk6130814@gmail.com>
2021-12-28 16:33:33 +0530

    final commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-window.onscroll = () =>{
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-    if(window.scrollY > 0){
-        document.querySelector('.header').classList.add('active');
-     }else{
-        document.querySelector('.header').classList.remove('active');
-     }
-};
-
+let menu=document.querySelector("#menu-btn"),navbar=document.querySelector(".header .navbar");menu.onclick=(()=>{menu.classList.toggle("fa-times"),navbar.classList.toggle("active")}),window.onscroll=(()=>{menu.classList.remove("fa-times"),navbar.classList.remove("active"),window.scrollY>0?document.querySelector(".header").classList.add("active"):document.querySelector(".header").classList.remove("active")});
